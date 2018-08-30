2908ca53ccd8a45f15b66e71d210cf11c287f6a7
vitali <pedalka@gmail.com>
2018-08-30 14:18:10 +0300

    add button with action change background

--- firstproject/ViewController.swift
+++ firstproject/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeBakround(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
