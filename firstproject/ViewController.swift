cdd177c72916ddfa9b66407fe76f114694eb813b
vitali <pedalka@gmail.com>
2018-09-03 09:14:35 +0300

    add table view with 3 sections

--- firstproject/ViewController.swift
+++ firstproject/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var basicLable: UILabel!
+    @IBAction func changeLable(_ sender: Any) {
+        basicLable.text = "Exciting new value"
+    }
+    
     @IBAction func changeBakround(_ sender: Any) {
         view.backgroundColor = UIColor.darkGray
+        
+        //TODO: change the text color of evry lable
+        
+        //get everything contained in the top level view
+        let evrething = view.subviews
+        
+        for eachView in evrething {
+            //is it a lable?
+            if eachView is UILabel
+            {
+                //downcast as UILable
+                let eachLable = eachView as! UILabel
+                eachLable.textColor = UIColor.green
+            }
+            
+        }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,7 +44,77 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    let dayliTasks = ["Check all windows",
+                      "Check all doors",
+                      "Is the boiler fueled?",
+                      "Check the mailbox",
+                      "Empty trash containers",
+                      "If freezing, check water pipes",
+                      "Document \"strang and unusual\" occurrences"]
+    
+    let weeklyTasks = ["Check inside all cabins",
+                       "Flush all lavatories in cabins",
+                       "Walk the perimeter of property"]
+    
+    let monthlyTasks = ["Test security alarm",
+                        "Test motion detector",
+                        "Test smoke alarms"]
+    
+    //-----------------------------------------------------------------------------------
+    // UITableViewDataSource - implementation
+    //-----------------------------------------------------------------------------------
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return dayliTasks.count
+        case 1:
+            return weeklyTasks.count
+        case 2:
+            return monthlyTasks.count
+            
+        default:
+            return 0
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is the \(indexPath.row)"
+        
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = dayliTasks[indexPath.row]
+        case 1:
+            cell.textLabel?.text = weeklyTasks[indexPath.row]
+        case 2:
+            cell.textLabel?.text = monthlyTasks[indexPath.row]
+        default:
+            cell.textLabel?.text = "This shouldn't happen"
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        case 2:
+            return "Monthly Tasks"
+        default:
+            return nil
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
 
 }
 
