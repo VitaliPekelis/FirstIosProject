f000403f025b0170960f7ca60270779226f03dad
vitali <pedalka@gmail.com>
2018-10-09 16:38:15 +0300

    Add Navigation to app

---
+++ firstproject/SecondScreenViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondScreenViewController.swift
+//  firstproject
+//
+//  Created by Vitali Pekelis on 09/10/2018.
+//  Copyright © 2018 my. All rights reserved.
+//
+
+import UIKit
+
+class SecondScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Second Screen"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
